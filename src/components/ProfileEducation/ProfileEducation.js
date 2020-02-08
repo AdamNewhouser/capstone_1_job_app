458cfc681110e5106e525aaf9424aab2278777b1
Adam Newhouser <adamnewhouser@gmail.com>
2020-02-08 13:10:52 -0500

    fixed

--- src/components/ProfileEducation/ProfileEducation.js
+++ src/components/ProfileEducation/ProfileEducation.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit } from '@fortawesome/free-solid-svg-icons'
 import './ProfileEducation.css'
 
 export default class ProfileEducation extends React.Component {
@@ -8,9 +6,7 @@ export default class ProfileEducation extends React.Component {
     render() {
     return (
         <div className='profile_education'>
-            <h3 className='ed_title'>Education
-            <FontAwesomeIcon icon={faEdit} className='edit_education_icon' />
-            </h3>
+            <h3 className='ed_title'>Education</h3>
             <h4 className='ed_h4'>{this.props.profile.school_name}</h4>
             <p className='ed ed_degree'>{this.props.profile.degree}</p>
             <p className='ed ed_length'>{this.props.profile.length_of_enrollment}</p>
